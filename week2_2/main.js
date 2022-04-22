c708e030e55cee91aafed2913a2fb0f12ed38d3f
Hennization <shy6295@ewhain.net>
2022-04-23 01:51:33 +0900

    feat: 장바구니 비우기 기능

--- week2_2/main.js
+++ week2_2/main.js
@@ -2,11 +2,18 @@ const $ = (selector) => document.querySelector(selector);
 const $All = (selector) => document.querySelectorAll(selector);
 const cartListWrapper = $(".main__cart-list-wrapper");
 
-function addBurgerToCart(taretBurgerName, targetBurgerPrice) {
+function emptyAllInCart() {
+  while (cartListWrapper.hasChildNodes()) {
+    cartListWrapper.removeChild(cartListWrapper.firstChild);
+  }
+}
+
+function addBurgerToCart(targetBurgerName, targetBurgerPrice) {
   const cartList = document.createElement("li");
+
   cartList.classList.add("main__cart-list");
   cartList.innerHTML = `
-    <strong class="main__cart-list-name">${taretBurgerName}</strong>
+    <strong class="main__cart-list-name">${targetBurgerName}</strong>
     <input type="number" value="1" min="1" class="main__cart-list-amount"/>
     <p class="main__cart-list-price">${targetBurgerPrice}</p>
     <button type="button" class="main__cart-list-deleteBtn">❌</button>
@@ -14,9 +21,8 @@ function addBurgerToCart(taretBurgerName, targetBurgerPrice) {
   cartListWrapper.appendChild(cartList);
 }
 
-function attachEvent({ burgerCardNodeList }) {
+function attachEvent({ burgerCardNodeList, emptyCartButton }) {
   burgerCardNodeList.addEventListener("click", (e) => {
-    // 장바구니에 버거 추가
     if (!e.target.closest(".burger__card")) return;
 
     const burgerCard = e.target.closest(".burger__card");
@@ -24,13 +30,18 @@ function attachEvent({ burgerCardNodeList }) {
     const burgerName = burgerContents.children[0].innerText;
     const burgerPrice = burgerContents.children[1].innerText;
 
+    // 장바구니에 버거 추가
     addBurgerToCart(burgerName, burgerPrice);
+  });
+
+  // 장바구니 비우기
+  emptyCartButton.addEventListener("click", (e) => {
+    emptyAllInCart();
   });
 }
 
 // 장바구니 관리 함수
 function cartManager(burgerInfo) {
-  // initCart(burgerInfo);
   attachEvent(burgerInfo);
 }
 
@@ -38,5 +49,6 @@ function cartManager(burgerInfo) {
 window.onload = () => {
   cartManager({
     burgerCardNodeList: $(".burger"),
+    emptyCartButton: $(".main__cart-cancelBtn"),
   });
 };
