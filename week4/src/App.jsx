3921c01c30efe7d93951f1aa3f1952f1cfa7dcc5
Hennization <shy6295@ewhain.net>
2022-05-29 19:45:36 +0900

    init: 초기세팅

--- week4/src/App.jsx
+++ week4/src/App.jsx
@@ -1,45 +1,16 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
+import Main from 'pages/Main';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'styles/globalStyle';
+import { theme } from 'styles/theme';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Main />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ week4/src/styles/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  fontSizes: {
+    form: '40px',
+    searchTitle: '34px',
+    input: '28px',
+    placeName: '40px',
+    roadAddressName: '30px',
+    phone: '30px',
+  },
+  colors: {
+    black: '#333333',
+    white: '#ffffff',
+    yellow: '#FFD951',
+    gray: '#6B7280',
+    skeleton: '#808080',
+  },
+};
