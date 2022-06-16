622fd341b5f0f2c2c653cb5028c39501072d35b7
Hennization <shy6295@ewhain.net>
2022-06-16 17:18:46 +0900

    fix: 조건문 수정

--- week3_ts/src/pages/Main.tsx
+++ week3_ts/src/pages/Main.tsx
@@ -21,15 +21,19 @@ export default function Main() {
     };
 
     // 화면이 리렌더링 될 때 마다 참가자들 배열과 승리자들 배열 확인
-    fighterList.length === 0 &&
-        useEffect(() => {
-            if (matchWinners.current.length >= 8) checkLists("8강");
-            else if (matchWinners.current.length >= 4) checkLists("4강");
-            else if (matchWinners.current.length >= 2) checkLists("결승");
-            else if (matchWinners.current.length === 1) {
-                navigation("/complete", { state: {checkLists(), matchWinners} });
-            }
-        });
+
+    useEffect(() => {
+        if (fighterList.length === 0 && matchWinners.current.length >= 8) checkLists("8강");
+        else if (fighterList.length === 0 && matchWinners.current.length >= 4) checkLists("4강");
+        else if (fighterList.length === 0 && matchWinners.current.length >= 2) checkLists("결승");
+        else if (fighterList.length === 0 && matchWinners.current.length === 1) {
+            console.log(matchWinners.current[0]);
+            navigation("/complete", {
+                // navigation에 값을 다중으로 넘겨주는 방법
+                state: { checkLists: checkLists("16강"), matchWinners: matchWinners },
+            });
+        }
+    });
 
     return (
         <StyledRoot>
